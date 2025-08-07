c8a1832c2c7279e30016f4e1fd5f994225d3f880
Brajesh <281092322419e@gmail.com>
2025-08-07 22:50:56 +0530

    complete login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import Messages from "./pages/Messages";
+import ChatBox from "./pages/ChatBox";
+import Connections from "./pages/Connections";
+import Discover from "./pages/Discover";
+import Profile from "./pages/Profile";
+import CreatePost from "./pages/CreatePost";
+import {useUser} from "@clerk/clerk-react"
+import { Layout } from "lucide-react";
+
 
 const App = () => {
-  return <div></div>;
+  const {user} = useUser()
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={!user ? <Login />: <Layout/>}>
+          <Route index element={<Feed />} />
+          <Route path="messags" element={<Messages />} />
+          <Route path="messags/:userId" element={<ChatBox />} />
+          <Route path="connections" element={<Connections />} />
+          <Route path="discover" element={<Discover />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/:profileId" element={<Profile />} />
+          <Route path="create-post" element={<CreatePost />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
