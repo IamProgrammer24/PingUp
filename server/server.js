3cf6e247e64296d3ae1f8cb1789b7997e6139e60
Brajesh <281092322419e@gmail.com>
2025-08-26 09:51:37 +0530

    add: vercel.json file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { serve } from "inngest/express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 const PORT = process.env.PORT || 4000;
 
